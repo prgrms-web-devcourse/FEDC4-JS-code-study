e0bbd171dca9352e003d754b0b4a5dc10480f849
oaoong <pizzachicken@kakao.com>
2023-06-15 15:41:00 +0900

    [PGS] 입국심사 / LV 3 / 50m

--- jaehee/입국심사_jaehee.js
+++ jaehee/입국심사_jaehee.js
@@ -5,6 +5,7 @@ function solution(n, times) {
   let answer = end;
 
   while (start <= end) {
+    // 탈출 조건
     const mid = Math.floor((start + end) / 2);
     let count = 0;
     times.forEach((v) => {
@@ -25,4 +26,17 @@ function solution(n, times) {
   return answer;
 }
 
-// 50m
+/*
+1. 알고리즘 or 자료구조 선택 이유
+문제의 주어진 조건이 엄격하기 때문에 logN의 시간복잡도를 가지는 이분탐색을 사용했습니다.
+
+2. 시간 복잡도 or 결과
+O(MlogN) (M: times의 길이, N: 시간 최대값)
+
+3. 기타 의견 
+수업 중에 나왔을 때도 틀렸던 문제인데 이번에도 한참 헤맸습니다. 이분탐색을 사용하는 것은 알았지만
+파라메트릭 서치로 찾는 값의 기준을 바꾸는 것이 아직도 익숙하지 않은 것 같습니다. 관련 문제를 더 풀어봐야겠습니다.
+
+4. 참고 링크
+
+*/
