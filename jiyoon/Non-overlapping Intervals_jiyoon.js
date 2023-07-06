6c4760a46bfce39d3de4b8ad517e5b7f22288344
JiYoon <angella990825@gmail.com>
2023-07-06 10:09:32 +0900

    [FIXED] [LTC] Non-overlapping Intervals / medium / 28m

--- jiyoon/Non-overlapping Intervals_jiyoon.js
+++ jiyoon/Non-overlapping Intervals_jiyoon.js
@@ -20,9 +20,9 @@ const eraseOverlapIntervals = function (intervals) {
 /*
 1. 알고리즘 or 자료구조 선택 이유
 그리디
-1. end를 기준으로 오름차순 정렬한다.
-2. stack에 intervals의 첫 번째 값을 넣어 준다.
-3. stack의 마지막 요소 end와 intervals의 start 값을 비교한다.
+1. end를 기준으로 오름차순 정렬
+2. stack에 intervals의 첫 번째 값을 넣음
+3. stack의 마지막 요소 end와 intervals의 start 값을 비교
 3-1. stack의 end가 intervals의 start보다 크면 cnt 증가
 3-2. 같거나 작으면 push
 
