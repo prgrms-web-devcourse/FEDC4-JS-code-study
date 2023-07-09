3299d8ddbba6b53dd97586a3cd4d634ef17c4a9d
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-07-09 18:54:58 +0900

    [LTC]/medium/2h

--- jisung/push_dominoes_jisung.js
+++ jisung/push_dominoes_jisung.js
@@ -3,36 +3,18 @@
  * @return {string}
  */
 const pushDominoes = function (dominoes) {
-  while (true) {
-    const changed = dominoes
-      .split("R.L")
-      .map((i) => i.replaceAll(".L", "LL").replaceAll("R.", "RR"))
-      .join("R.L");
-
-    if (changed === dominoes) break;
-    dominoes = changed;
-  }
-
-  return dominoes;
+  // 이 문제는 풀지 못 했습니다........
 };
 
 /*
 1. 알고리즘 or 자료구조 선택 이유
-DP인가 ,,?
+- L과 R에서부터 시작되는 경우의 수를 이용한 문제인 건 충분히 이해했으나 다 풀지는 못 했습니다. 
 
 2. 시간 복잡도 or 결과
-runtime: 334 ms / beats 36.74%
-memory: 57.6 mb / beats 91.84%
 
 3. 기타 의견
-문제 풀고 나서 다른 사람들의 풀이를 보니까,
-문제에서 의도한 DP 풀이와는 다르게 푼 것 같습니다 ㅠ
-
-문제에서 의도한 DP: 'L' or 'R' 한 쌍 기준으로 쪼개서 풀이
-내가 푼 DP: 시간 초마다 쪼개고 ( ... ) 'L.R' 기준으로 쪼개서 풀이
-
-제가 푼 방식은 두 개의 기준으로 쪼개서 시간복잡도가 더 걸릴 수 밖에 없었네용 ..
-하지만 가독성은 더 좋으니(?) 합리화를 해보겠습니다.
+- dp문제를 파악하는 방식을 알게되어서 오히려 기뻤습니다!!!!! 
+- 다만 구현능력을 강화하는게 큰 숙제인 것 같습니다.
 
 4. 참고 링크
 */
