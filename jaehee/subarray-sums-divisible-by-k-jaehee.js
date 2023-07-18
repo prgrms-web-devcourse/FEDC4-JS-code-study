63635f7ef405850355c002dd4723d97469e6bec2
oaoong <pizzachicken@kakao.com>
2023-07-18 15:30:54 +0900

    [LTC] subarray sums divisible by k / medium / 38m / x

---
+++ jaehee/subarray-sums-divisible-by-k-jaehee.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraysDivByK = function (nums, k) {};
+
+// 38m
+
+/*
+1. 알고리즘 or 자료구조 선택 이유
+
+2. 시간 복잡도 or 결과
+
+3. 기타 의견 
+풀 수 있을 것 같았는데... 방법이 안 떠오르네요?
+공부해오겠습니다
+
+4. 참고 링크
+
+*/
