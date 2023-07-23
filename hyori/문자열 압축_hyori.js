813661001ffff15d36a0c0687566cc8103bb100f
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2023-07-24 07:41:03 +0900

    [PGS] 문자열 압축 / Lv2 / 1h

--- hyori/문자열 압축_hyori.js
+++ hyori/문자열 압축_hyori.js
@@ -1,33 +1,33 @@
 function solution(s) {
-	let result = s.length
+  let result = s.length
 
-	for (let range = 1; range <= s.length / 2; range++) {
-		let cntLen = s.length
-		let cntNum = 0
-		let lastStr = ''
+  for (let range = 1; range <= s.length / 2; range++) {
+    let cntLen = s.length
+    let cntNum = 0
+    let lastStr = ''
 
-		for (let idx = 0; idx + range <= s.length; idx += range) {
-			const str = s.substring(idx, idx + range)
+    for (let idx = 0; idx + range <= s.length; idx += range) {
+      const str = s.substring(idx, idx + range)
 
-			cntNum++
-			if (lastStr !== str) {
-				if (cntNum > 1) {
-					cntLen -= cntNum * range - (cntNum.toString().length + range)
-				}
+      cntNum++
+      if (lastStr !== str) {
+        if (cntNum > 1) {
+          cntLen -= cntNum * range - (cntNum.toString().length + range)
+        }
 
-				lastStr = str
-				cntNum = 0
-			}
-		}
-		if (cntNum > 0) {
-			cntNum++
-			cntLen -= cntNum * range - (cntNum.toString().length + range)
-		}
+        lastStr = str
+        cntNum = 0
+      }
+    }
+    if (cntNum > 0) {
+      cntNum++
+      cntLen -= cntNum * range - (cntNum.toString().length + range)
+    }
 
-		result = Math.min(result, cntLen)
-	}
+    result = Math.min(result, cntLen)
+  }
 
-	return result
+  return result
 }
 
 /*
