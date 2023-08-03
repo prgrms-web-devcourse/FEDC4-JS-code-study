432f12b31a094c702c40ed963015520c89994910
oaoong <pizzachicken@kakao.com>
2023-08-03 14:55:02 +0900

    [LTC] student attendance record 2 / hard / / X

---
+++ jaehee/2308/student-attendance-record-2-jaehee.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var checkRecord = function (n) {};
+
+/*
+1. 알고리즘 or 자료구조 선택 이유
+
+2. 시간 복잡도 or 결과
+
+3. 기타 의견 
+점화식이 너무 끔찍해서 머리가 거부해요 살려주세요
+
+4. 참고 링크
+
+*/
