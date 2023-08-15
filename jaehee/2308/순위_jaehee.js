b978e706d5162a438d6176841fe5809bdaa3b958
oaoong <pizzachicken@kakao.com>
2023-08-15 16:24:53 +0900

    [PGS] 순위 / Lv 3 / 52m / x

---
+++ jaehee/2308/순위_jaehee.js
@@ -0,0 +1,16 @@
+function solution(n, results) {
+  var answer = 0;
+  return answer;
+}
+
+/*
+1. 알고리즘 or 자료구조 선택 이유
+
+2. 시간 복잡도 or 결과
+
+3. 기타 의견 
+플로이드 워셜 문제인 것 같아 만만하게 생각했는데 못 풀었습니다...?
+
+4. 참고 링크
+
+*/
