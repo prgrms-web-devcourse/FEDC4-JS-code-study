caa1f61175be9774e28431a1fcfdbbe100eaf2ef
oaoong <pizzachicken@kakao.com>
2024-03-06 17:52:21 +0900

    PGS / Lv 3 / 40m / x

---
+++ jaehee/2403/숫자-타자-대회.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+  let answer = 0;
+  let l = 4;
+  let r = 6;
+
+  for (let number of numbers.split("")) {
+    const target = number.toString();
+  }
+
+  return answer;
+}
+
+// DP로 풀어야 할 것 같은데..!
